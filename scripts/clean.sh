4f1cfab81bee9e7260cffe2bd973a5765749b480
George Campbell <geocolumbus@gmail.com>
2016-11-13 13:31:11 -0500

    Add clean and install features to the stack.

---
+++ scripts/clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+rm -rf www/js/lib
+mkdir www/js/lib
+rm -rf www/css/lib
+mkdir www/css/lib
+find . -name \"npm-debug.log\" -type f -delete
---
+++ scripts/copy.js
@@ -0,0 +1,51 @@
+var fs = require('fs'),
+    files = [{
+        "source": "jquery/dist/jquery.min.js",
+        "name": "jquery"
+    }, {
+        "source": "requirejs/require.js",
+        "name": "requirejs"
+    }, {
+        "source": "bootstrap/dist/js/bootstrap.min.js",
+        "name": "bootstrap"
+    }, {
+        "source": "bootstrap/dist/css/bootstrap.min.css",
+        "name": "bootstrap"
+    }];
+
+files.forEach(function(file) {
+    if (file.source.substr(file.source.length - 2, 2) === "js") {
+        copyFile('node_modules/' + file.source, 'www/js/lib/' + file.name + '.js', function() {
+            console.log('copied ' + file.name + '.js to www/js/lib folder.')
+        });
+    } else {
+        copyFile('node_modules/' + file.source, 'www/css/lib/' + file.name + '.css', function() {
+            console.log('copied ' + file.name + '.css to www/css/lib folder.')
+        });
+    }
+});
+
+// Copy a file
+function copyFile(source, target, cb) {
+    var cbCalled = false;
+
+    var rd = fs.createReadStream(source);
+    rd.on("error", function(err) {
+        done(err);
+    });
+    var wr = fs.createWriteStream(target);
+    wr.on("error", function(err) {
+        done(err);
+    });
+    wr.on("close", function(ex) {
+        done();
+    });
+    rd.pipe(wr);
+
+    function done(err) {
+        if (!cbCalled) {
+            cb(err);
+            cbCalled = true;
+        }
+    }
+}
